ae88ffe5c29f73ece794e1b3cf67f40c58f427a5
Borisisichec <134488260+Borisisichec@users.noreply.github.com>
2025-01-17 20:36:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -350,8 +350,4 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 
-    app.run_polling()
-
-
-if __name__ == "__main__":
-    main()
+tgbot = TGBot(router)
